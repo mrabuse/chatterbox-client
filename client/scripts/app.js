d82e3ce090b85a118e2998d002e44de247c38ea9
James Kip <jameskip@me.com>
2017-02-13 13:18:28 -0600

    Implement - project outline

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,13 +1,19 @@
-// YOUR CODE HERE:
+//  DESCRIBE 'chatterbox' //
+
 const app = {
 
 };
 
+// DESCRIBE 'init'  //
+
 app.init = function () {};
 
+//  DESCRIBE 'app behavior' //
+
+// DESCRIBE 'sending'   //
 app.send = function(message) {
   $.ajax({
-    url: "http://parse.atx.hackreactor.com/chatterbox/classes/messages",
+    url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
     data: JSON.stringify(message),
     contextType: 'application/json',
@@ -20,15 +26,41 @@ app.send = function(message) {
   });
 };
 
+//  DESCRIBE 'fething'  //
 app.fetch = function(string) {
   $.ajax({
-    server: "http://parse.atx.hackreactor.com/chatterbox/classes/messages",
+    server: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
     type: 'GET',
     success: function () {
       console.log('Success');
     },
-    error: function (){
+    error: function () {
       console.log('chatterbox: Failed to get');
     }
   });
+};
+
+//  TODO DESCRIBE 'chatroom behavior'  //
+
+app.clearMessages = function() {
+  // TODO: should be able to clear messages from the DOM
+};
+
+app.renderMessage = function(message) {
+  // TODO: should be able to add messages to the DOM
+};
+
+app.renderRoom = function(room) {
+  // TODO: should be able to add rooms to the DOM
+};
+
+
+//  TODO DESCRIBE 'events'  //
+
+app.handleUsernameClick = function() {
+  // TODO: should add a friend upon clicking their username
+};
+
+app.handleSubmit = function() {
+  // TODO: should try to send a message upon clicking submit
 };
