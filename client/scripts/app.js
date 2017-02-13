193b598a6492716a9062352ed4151e9103e1f983
mrabuse <cogglemat@gmail.com>
2017-02-13 12:28:11 -0600

    Implement - chatterbox (init, sending, fetching)
    passes: 7 failures: 5

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,34 @@
 // YOUR CODE HERE:
+const app = {
+
+};
+
+app.init = function () {};
+
+app.send = function(message) {
+  $.ajax({
+    url: "http://parse.atx.hackreactor.com/chatterbox/classes/messages",
+    type: 'POST',
+    data: JSON.stringify(message),
+    contextType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.log('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function(string) {
+  $.ajax({
+    server: "http://parse.atx.hackreactor.com/chatterbox/classes/messages",
+    type: 'GET',
+    success: function () {
+      console.log('Success');
+    },
+    error: function (){
+      console.log('chatterbox: Failed to get');
+    }
+  });
+};
